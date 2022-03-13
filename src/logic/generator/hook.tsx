72c9111ea593d8fe2c3b21c0db1898177cfd6818
thailonlucas <thailon.lucas28@gmail.com>
2022-03-13 16:51:04 -0300

    New suggestions

--- src/logic/generator/hook.tsx
+++ src/logic/generator/hook.tsx
@@ -2,9 +2,9 @@ import { useState } from "react"
 import { RandomGenerator } from "./controller"
 
 export const useRandomGenerator = () => {
-    const [colorPallete, setColorPalette] = useState(['#FFF', '#FFF', '#FFF'])
-    const [theme, setTheme] = useState('Criatividade')
     const randomGenerator = RandomGenerator()
+    const [colorPallete, setColorPalette] = useState(randomGenerator.getColorPallete())
+    const [theme, setTheme] = useState('Criatividade')
 
     const generateTheme = () => {
         setTheme(randomGenerator.getTheme())
