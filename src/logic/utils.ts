6735dc06f432fa59bf1e390d3f39e1bc9da590f4
thailonlucas <thailon.lucas28@gmail.com>
2022-03-13 16:41:30 -0300

    Fix random logic

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -1,9 +1,3 @@
-export const getRandomArbitrary = (min: number, max: number) => {
-    let randomNumber:any = Number.parseInt((Math.random() * ((max + 1) - min) + min))
-    while(randomNumber > max){
-        randomNumber = getRandomArbitrary(min, max)
-    }
-    return randomNumber
-}
+export const getRandomArbitrary = (min: number, max: number) => Number.parseInt((Math.random() * ((max + 1) - min) + min))
 
 export const getRandomListItem = (list: any) => list[getRandomArbitrary(0,(list.length - 1))]
