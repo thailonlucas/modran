4da9793dc99fcb5c1902df4de2c705f64c8655d2
thailonlucas <thailon.lucas28@gmail.com>
2022-03-15 21:18:29 -0300

    Misspeling app name

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.scss';
 import ThemeGenerator from './pages/ThemeGenerator';
-import MondranLogo from './resources/img/logo.svg';
+import modranLogo from './resources/img/logo.svg';
 
 function App() {
   return (
     <>
     <header>
-      <img style={{width:90}} src={MondranLogo} alt='logo da mondran'/>
+      <img style={{width:90}} src={modranLogo} alt='logo da modran'/>
       <div className='creator-info'>
         <h1>
           made by: <a target='_blank' rel="noreferrer" href='https://www.instagram.com/thailonlucas'>@thailonlucas</a>
